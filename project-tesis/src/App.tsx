92d3c3306f5261cff6e8124c2f25d90d035a9ef1
AstaGhost13 <Sanchezandra2002@hotmail.com>
2025-08-21 22:14:43 -0500

    configuracion herramientas

--- project-tesis/src/App.tsx
+++ project-tesis/src/App.tsx
@@ -1,9 +1,10 @@
-import "./App.css";
 
+
+import Login from "./components/Login";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-amber-300">Hello world!</h1>
+      <Login />
     </>
   );
 }
